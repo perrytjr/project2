ac5d0768c0b6988521283b4a713b57ee85ad9ebc
Perry Theobald <perrytjr@gmail.com>
2020-10-07 09:34:36 -0700

    added code to config/connectionjs created db folder with schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+### Schema
+
+CREATE DATABASE chatroom;
+
+USE chatroom;
+
+--CREATE TABLE   Thursday we can see how we want to set up the tables for the database 
+-- going off the sign in page will have to store their Full Name, Username, Bio, and then their Posts they make in the chat?-
