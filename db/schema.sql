60e2f270d3b093083ef4fb01e0d8d645ff5e649e
msurio <suriomanthony@gmail.com>
2020-10-08 21:04:57 -0700

    marcAnthony changes on the main.js, chat.html, and index.html

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 ### Schema
 
-<<<<<<< HEAD
 
 DROP DATABASE IF EXISTS chat_roomDB;
 
@@ -9,7 +8,7 @@ CREATE DATABASE chat_roomDB;
 USE chat_roomDB;
 --will we prompt the user for hobbies/short story? how will we do so. How will we connect the user info to mysql?
 CREATE TABLE users (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   fullname VARCHAR(45) NULL,
   username VARCHAR(45) NULL,
   PRIMARY KEY (id)
@@ -23,11 +22,3 @@ VALUES ("Bob", "Bobby")
 
 --CREATE TABLE  we have to think about how we want to create our table. Do we only need one that stores, Full Name,
 --User Name, Bio, and their Posts?
-=======
-CREATE DATABASE chatroom;
-
-USE chatroom;
-
---CREATE TABLE   Thursday we can see how we want to set up the tables for the database 
--- going off the sign in page will have to store their Full Name, Username, Bio, and then their Posts they make in the chat?-
->>>>>>> perry
