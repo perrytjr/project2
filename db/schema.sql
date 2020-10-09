ee73b32e494223feb895e309cc0ca0e041eb9c14
Perry Theobald <perrytjr@gmail.com>
2020-10-08 21:11:25 -0700

    asking me to commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
 ### Schema
 
-CREATE DATABASE chatroom;
+CREATE DATABASE chat_roomDB;
 
-USE chatroom;
+USE chat_roomDB;
 
 --CREATE TABLE   Thursday we can see how we want to set up the tables for the database 
 -- going off the sign in page will have to store their Full Name, Username, Bio, and then their Posts they make in the chat?-
